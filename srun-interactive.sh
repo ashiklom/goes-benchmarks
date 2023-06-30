308f7f54e8e51d4d200a0c7d4be986ba5379c9b9
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2023-06-30 23:12:02 +0000

    Tweak srun script

--- srun-interactive.sh
+++ srun-interactive.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 # eso-c5n18xlarge-spot-dy-c5n18xlarge-[1-100]
-srun -c 36 -p eso-c5n18xlarge-demand --pty bash
+srun -c 36 -p eso-c5n18xlarge-spot,eso-c5n18xlarge-demand --pty bash
+# salloc -c 36 -p eso-c5n18xlarge-spot,eso-c5n18xlarge-demand
