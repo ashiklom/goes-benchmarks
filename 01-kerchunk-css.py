fc82bf5e826121439692c80f2ed4c516a0edf2dd
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2023-10-05 15:38:48 -0400

    Tweak CSS JSON file generation script

--- 01-kerchunk-css.py
+++ 01-kerchunk-css.py
@@ -1,37 +1,51 @@
 # List the files
 import glob
+import os
 
-base_path = '/css/geostationary/BackStage/GOES-17-ABI-L1B-FULLD/2022/'
+base_path = '/css/geostationary/BackStage/GOES-17-ABI-L1B-FULLD/2022/001'
 outdir = "./results/g17l1b"
 
-# all_paths = sorted(glob.glob(base_path + '*/*/*.nc'))
-with open('goesfiles', 'r') as f:
-    all_paths = f.read().splitlines()
+os.makedirs(outdir, exist_ok=True)
+
+# Cache the full list of GOES files --- otherwise, this takes a while
+# if os.path.exists('goesfiles'):
+#     with open('goesfiles', 'r') as f:
+#         all_paths = f.read().splitlines()
+# else:
+    # all_paths = sorted(glob.glob(base_path + '*/*/*.nc'))
+    # with open('goesfiles', 'w') as f:
+    #     f.write("\n".join(all_paths))
+
+all_paths = sorted(glob.glob(base_path + '*/*/*.nc'))
 all_paths[:5]
 all_paths[-5:]
 len(all_paths)
 
 import ujson
 from kerchunk.hdf import SingleHdf5ToZarr
-import os
-
-from dask.distributed import Client, LocalCluster, progress
-cluster = LocalCluster()
-client = Client(cluster)
-
-if not os.path.exists(outdir):
-    os.mkdir(outdir)
 
 def gen_json(f):
+    outf = f'{outdir}/{os.path.basename(f)}.json'
+    if os.path.exists(outf):
+        return outf
     try:
+        # Read the chunks locally (infile).
+        # But...actually point to the files on S3, assuming they are identical!
+        out_url = 's3://eso-west2-curated' + f
         with open(f, 'rb') as infile:
-            h5chunks = SingleHdf5ToZarr(infile, f)
-            outf = f'{outdir}/{os.path.basename(f)}.json'
+            h5chunks = SingleHdf5ToZarr(
+                infile,
+                out_url
+            )
             with open(outf, 'wb') as outfile:
                 outfile.write(ujson.dumps(h5chunks.translate()).encode())
             return outf
     except:
         return None
+
+from dask.distributed import Client, LocalCluster, progress
+cluster = LocalCluster()
+client = Client(cluster)
 
 import dask.bag as db
 
