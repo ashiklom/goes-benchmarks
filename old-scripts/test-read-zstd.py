3adb0dd9bd103fa50f9b7ce50ebd18648e658c7a
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2023-10-16 11:46:22 -0400

    Tweak test script

--- old-scripts/test-read-zstd.py
+++ old-scripts/test-read-zstd.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
 
 import xarray as xr
+import fsspec
 
-dat = xr.open_dataset("reference://", engine="zarr", backend_kwargs={
-    "consolidated": False,
-    "storage_options": {
-        "fo": "results/final-compressed/2022-001.json.zst",
-        "target_options": {"compression": 'zstd'}
-    }
-})
+from dask.distributed import Client, LocalCluster
+cluster = LocalCluster()
+client = Client(cluster)
+
+target = fsspec.filesystem("reference", fo="results/final/2022-002.json").get_mapper()
+# target = fsspec.filesystem(
+#     "reference",
+#     "fo": "results/final-compressed/2022-002.json.zst",
+#     "compression": 'zstd'
+# )
+dat = xr.open_zarr(target, consolidated=False)
 
 # Try calculating a global mean
+%time globmean = dat.isel(t=slice(200, 300)).Rad.mean(["t"]).values()
+globmean.values()
+
 %time globmean = dat.Rad.mean(["t"])
