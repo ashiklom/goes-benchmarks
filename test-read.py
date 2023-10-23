0f81d30c779413d7c261ace58431c54fa331936b
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2023-10-23 14:15:42 -0400

    Scratch code for testing read

--- test-read.py
+++ test-read.py
@@ -3,11 +3,11 @@
 import xarray as xr
 import fsspec
 
-from dask.distributed import Client, LocalCluster, progress
-cluster = LocalCluster()
-client = Client(cluster)
+# from dask.distributed import Client, LocalCluster, progress
+# cluster = LocalCluster()
+# client = Client(cluster)
 
-target = fsspec.filesystem("reference", fo="results/final/2022-002.json").get_mapper()
+target = fsspec.filesystem("reference", fo="results/final/2022-001.json").get_mapper()
 # target = fsspec.filesystem(
 #     "reference",
 #     fo="results/final-compressed/2022-002.json.zst",
@@ -15,13 +15,22 @@ target = fsspec.filesystem("reference", fo="results/final/2022-002.json").get_ma
 # )
 dat = xr.open_zarr(target, consolidated=False)
 
+# Does this work? No idea..
+# targets = [fsspec.filesystem("reference", fo=f).get_mapper() for f in json_files]
+# datmf = xr.open_mfdataset(targets)
+
 # Try extracting a time series for a random location
-tseries = dat.isel(x=5000, y=5000, t=slice(0, 100)).Rad
+dat
+tseries = dat.isel(bandn=1, x=4000, y=7000, time=slice(50,100)).Rad
+tseries.values
+
 result = tseries.persist()
 progress(result)
+# ~70 sec to read 100 time steps
 
 # Try calculating a global mean
-%time globmean = dat.isel(t=slice(200, 300)).Rad.mean(["t"]).values()
-globmean.values()
+globmean = dat.isel(t=slice(200, 300)).Rad.mean(["t"])
+rmean = globmean.persist()
+progress(rmean)
 
 %time globmean = dat.Rad.mean(["t"])
