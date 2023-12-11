152a725d1bfa57dc73497eb489a7442bfd515b9a
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2023-12-11 13:37:24 -0500

    Tweak test script

--- test-read.py
+++ test-read.py
@@ -3,9 +3,9 @@
 import xarray as xr
 import fsspec
 
-# from dask.distributed import Client, LocalCluster, progress
-# cluster = LocalCluster()
-# client = Client(cluster)
+from dask.distributed import Client, LocalCluster, progress
+cluster = LocalCluster()
+client = Client(cluster)
 
 target = fsspec.filesystem("reference", fo="results/final/2022-001.json").get_mapper()
 # target = fsspec.filesystem(
@@ -20,17 +20,30 @@ dat = xr.open_zarr(target, consolidated=False)
 # datmf = xr.open_mfdataset(targets)
 
 # Try extracting a time series for a random location
-dat
 tseries = dat.isel(bandn=1, x=4000, y=7000, time=slice(50,100)).Rad
-tseries.values
+
+# 50 time steps for 1 band
+%time tseries.values
+# CPU times: user 2.41 s, sys: 327 ms, total: 2.74 s
+# Wall time: 3.24 s
+
+# 1 day's worth of data for one band and location
+%time dat.isel(bandn=2, x=4000, y=7000).Rad.values
+# CPU times: user 2.41 s, sys: 327 ms, total: 2.74 s
+# Wall time: 3.24 s
 
 result = tseries.persist()
 progress(result)
 # ~70 sec to read 100 time steps
 
-# Try calculating a global mean
-globmean = dat.isel(t=slice(200, 300)).Rad.mean(["t"])
+# Try calculating a global mean for a single timestep
+globmean = dat.isel(time=0).Rad.mean(["bandn"])
 rmean = globmean.persist()
+progress(rmean)
+
+# Try calculating a global mean time series for the entire day
+dailymean = dat.Rad.isel(bandn=0).mean(["x", "y"])
+rmean = dailymean.persist()
 progress(rmean)
 
 %time globmean = dat.Rad.mean(["t"])
